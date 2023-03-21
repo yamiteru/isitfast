77be813ab3c87aad6608d6fb7665a29d95fa58e5
yamiteru <yamiteru@icloud.com>
2023-03-21 23:46:24 +0100

    Update build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,39 +1,31 @@
 import { build, BuildOptions } from "esbuild";
-import { Generator } from "npm-dts";
-import { source, main as cjs, module as esm, types } from "../package.json";
 import { swcPlugin } from "esbuild-plugin-swc";
 
-const shared: BuildOptions = {
+const shared = {
   bundle: true,
-  entryPoints: [source],
+  entryPoints: ["src/index.ts"],
   plugins: [swcPlugin()],
   logLevel: "info",
   minify: true,
-  sourcemap: true,
-};
-
-const buildEsm = () =>
-  build({
-    ...shared,
-    format: "esm",
-    outfile: esm,
-    target: ["esnext", "node18.0.0"],
-  });
-
-const buildCjs = () =>
-  build({
-    ...shared,
-    format: "cjs",
-    outfile: cjs,
-    target: ["esnext", "node18.0.0"],
-  });
-
-const buildTypes = () =>
-  new Generator({
-    entry: source,
-    output: types,
-  }).generate();
+  platform: "node",
+  sourcemap: "linked",
+  treeShaking: true,
+  outdir: "dist",
+  target: ["esnext", "node18.0.0"],
+} as BuildOptions ;
 
 (async () => {
-  await Promise.all([buildEsm(), buildCjs(), buildTypes()]);
+  await Promise.all([
+    build({
+      ...shared,
+      format: "esm",
+      outExtension: { ".js": ".esm.js" },
+      splitting: true,
+    }),
+    build({
+      ...shared,
+      format: "cjs",
+      outExtension: { ".js": ".cjs.js" },
+    }),
+  ]);
 })();
