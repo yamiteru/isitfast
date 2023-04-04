8e238b7ab0b0801107f6fc78c6c05fa68815f308
yamiteru <yamiteru@icloud.com>
2023-04-04 19:51:47 +0200

    Remove unnecessary files

--- src/store/createStore.ts
+++
@@ -1,8 +0,0 @@
-import { Mode, Options } from "../types";
-
-export function createStore(mode: Mode, options: Options) {
-  return {
-    array: new Uint32Array(new ArrayBuffer(options[mode].chunkSize * 4)),
-    index: 0,
-  };
-}
