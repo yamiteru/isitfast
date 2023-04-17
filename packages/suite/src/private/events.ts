91804364e1f0a57b25936484d8a217736881ec2d
Yamiteru <yamiteru@icloud.com>
2023-04-17 18:34:43 +0200

    Extend events and their data

--- packages/suite/src/private/events.ts
+++ packages/suite/src/private/events.ts
@@ -1,5 +1,5 @@
 import {STATE} from "@isitfast/constants";
-import {Offset, Mode} from "@isitfast/types";
+import {Offset, Mode, Type} from "@isitfast/types";
 import { pub } from "ueve/async";
 import {$garbageStart, $garbageEnd, $suiteStart, $suiteEnd, $benchmarkStart, $benchmarkEnd, $offsetStart, $offsetEnd, $iterationStart, $iterationEnd} from "@isitfast/events";
 
@@ -26,13 +26,14 @@ export async function suiteEnd() {
   });
 }
 
-export async function benchmarkStart(name: string) {
+export async function benchmarkStart(name: string, type: Type) {
   await STATE.benchmarks[name]?.events?.onBenchmarkStart?.();
-  await pub($benchmarkStart, { suiteName: STATE.name, benchmarkName: name });
+  await pub($benchmarkStart, { suiteName: STATE.name, benchmarkName: name, type});
 }
 
 export async function benchmarkEnd(
   name: string,
+  type: Type,
   data: {
     cpu: Offset;
     ram: Offset;
@@ -42,29 +43,32 @@ export async function benchmarkEnd(
   await pub($benchmarkEnd, {
     suiteName: STATE.name,
     benchmarkName: name,
+    type,
     data,
   });
 }
 
-export async function offsetStart(name: string) {
-  await pub($offsetStart, { suiteName: STATE.name, offsetName: name });
+export async function offsetStart(name: string, type: Type, mode: Mode) {
+  await pub($offsetStart, { suiteName: STATE.name, offsetName: name, type, mode });
 }
 
-export async function offsetEnd(name: string, offset: Offset) {
-  await pub($offsetEnd, { suiteName: STATE.name, offsetName: name, offset });
+export async function offsetEnd(name: string, type: Type, mode: Mode, offset: Offset) {
+  await pub($offsetEnd, { suiteName: STATE.name, offsetName: name, type, mode, offset });
 }
 
-export async function iterationStart(name: string, mode: Mode) {
+export async function iterationStart(name: string, type: Type, mode: Mode) {
   await STATE.benchmarks[name]?.events?.onIterationStart?.();
   await pub($iterationStart, {
     suiteName: STATE.name,
     benchmarkName: name,
+    type,
     mode,
   });
 }
 
 export async function iterationEnd(
   name: string,
+  type: Type,
   mode: Mode,
   data: number,
   isGCFluke: boolean,
@@ -73,6 +77,7 @@ export async function iterationEnd(
   await pub($iterationEnd, {
     suiteName: STATE.name,
     benchmarkName: name,
+    type,
     mode,
     data,
     isGCFluke,
