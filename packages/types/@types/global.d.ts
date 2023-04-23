d38abb8a28017e2f108e84c825c8576b43d4fa3d
Yamiteru <yamiteru@icloud.com>
2023-04-24 00:19:29 +0200

    Update types

--- packages/types/@types/global.d.ts
+++ packages/types/@types/global.d.ts
@@ -1,11 +1,11 @@
 declare global {
   interface Performance {
     memory: {
-      /** The currently active segment of JS heap, in bytes. */
+      // The currently active segment of JS heap, in bytes.
       usedJSHeapSize: number;
     };
 
-    /** The currently active segment of JS heap, in bytes. */
+    // The currently active segment of JS heap, in bytes.
     measureUserAgentSpecificMemory: () => { bytes: number };
   }
 }
